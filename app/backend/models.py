9592a0c15d7e084a6a1721ee584d10f60d290c76
YastMe <ale4xd@hotmail.com>
2024-10-14 20:29:48 +0200

    Added db models

--- app/backend/models.py
+++ app/backend/models.py
@@ -8,9 +8,22 @@ class Person(models.Model):
 
 
 class User(AbstractUser):
+    username = models.TextField(unique=True, null=True)
+    password = models.TextField(unique=True, null=True)
     email = models.EmailField(unique=True)
     is_2fa_enabled = models.BooleanField(default=False)
     otp_secret = models.CharField(max_length=16, blank=True, null=True)  # OTP secret
+    last_online = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
+
+class Game(models.Model):
+    j1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='j1')
+    j2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='j1')
+    game = models.BinaryField()
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner')
+
+class FriendListLine(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend')
